20fb6fb8885798d1fdceb4c8dc9310ae3c23e126
skkzsh <skkzsh12@gmail.com>
2022-10-23 01:44:57 +0900

    Solve 4.5

--- my/q04_05.cpp
+++ my/q04_05.cpp
@@ -5,7 +5,35 @@ int count753(const int k) {
         throw std::invalid_argument("argument must not be negative");
     }
 
-    return k;
+    if (k < 357) {
+        return 0;
+    }
+
+    std::map<int, int> count753map = {
+        {3, 0},
+        {5, 0},
+        {7, 0},
+    };
+
+    for (int tail = k; tail > 0; tail /= 10) {
+        switch(tail % 10) {
+            case 3:
+            case 5:
+            case 7:
+                count753map[tail % 10]++;
+                break;
+            default:
+                return count753(k - 1);
+                break;
+        }
+    }
+
+    // if (std::all_of(count753map.begin(), count753map.end(),
+    //             [](std::map<int, int>::iterator x) { return (x -> second > 0); })) {
+    if (count753map[3] > 0 && count753map[5] > 0 && count753map[7] > 0) {
+        return count753(k - 1) + 1;
+    }
+    return count753(k - 1);
 }
 
 
@@ -23,6 +51,14 @@ TEST(TestCase, Test1Max) {
 
 TEST(TestCase, Test2Min) {
     EXPECT_EQ(count753(375), 2);
+}
+
+TEST(TestCase, Test4digit1) {
+    EXPECT_EQ(count753(1357), 6);
+}
+
+TEST(TestCase, Test4digitMin) {
+    EXPECT_EQ(count753(3357), 7);
 }
 
 TEST(TestCase, Negative) {
