06f9c163e81fedd399cd4a149e0fd482e8dd4b2d
skkzsh <skkzsh12@gmail.com>
2022-10-23 02:42:25 +0900

    Try to solve 4.5 by permutation

--- my/q04_05.cpp
+++ my/q04_05.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
-int count753(const int k) {
+// 3, 5, 7のカウントから算出する
+int count753recursive(const int k) {
     if (k < 0) {
         throw std::invalid_argument("argument must not be negative");
     }
@@ -23,7 +24,7 @@ int count753(const int k) {
                 count753map[tail % 10]++;
                 break;
             default:
-                return count753(k - 1);
+                return count753recursive(k - 1);
                 break;
         }
     }
@@ -31,37 +32,62 @@ int count753(const int k) {
     // if (std::all_of(count753map.begin(), count753map.end(),
     //             [](std::map<int, int>::iterator x) { return (x -> second > 0); })) {
     if (count753map[3] > 0 && count753map[5] > 0 && count753map[7] > 0) {
-        return count753(k - 1) + 1;
+        return count753recursive(k - 1) + 1;
     }
-    return count753(k - 1);
+    return count753recursive(k - 1);
+}
+
+// 3, 5, 7の順列から算出する
+int count753permutation(const int k) {
+    if (k < 0) {
+        throw std::invalid_argument("argument must not be negative");
+    }
+
+    std::vector<char> v = {'3', '5', '7'};
+    // int digit = std::to_string(k).length(); // TODO: 4ケタ以上の場合に対応する
+
+    std::vector<int> candidates;
+    do {
+        std::string str(v.begin(), v.end());
+        candidates.push_back(std::stoi(str));
+    } while (std::next_permutation(v.begin(), v.end()));
+
+    return std::count_if(candidates.begin(), candidates.end(), [k](int c) { return c <= k; });
 }
 
 
 TEST(TestCase, Test0) {
-    EXPECT_EQ(count753(356), 0);
+    EXPECT_EQ(count753recursive(356), 0);
+    EXPECT_EQ(count753permutation(356), 0);
 }
 
 TEST(TestCase, Test1Min) {
-    EXPECT_EQ(count753(357), 1);
+    EXPECT_EQ(count753recursive(357), 1);
+    EXPECT_EQ(count753permutation(357), 1);
 }
 
 TEST(TestCase, Test1Max) {
-    EXPECT_EQ(count753(374), 1);
+    EXPECT_EQ(count753recursive(374), 1);
+    EXPECT_EQ(count753permutation(374), 1);
 }
 
 TEST(TestCase, Test2Min) {
-    EXPECT_EQ(count753(375), 2);
+    EXPECT_EQ(count753recursive(375), 2);
+    EXPECT_EQ(count753permutation(375), 2);
 }
 
 TEST(TestCase, Test4digit1) {
-    EXPECT_EQ(count753(1357), 6);
+    EXPECT_EQ(count753recursive(1357), 6);
+    EXPECT_EQ(count753permutation(1357), 6);
 }
 
 TEST(TestCase, Test4digitMin) {
-    EXPECT_EQ(count753(3357), 7);
+    EXPECT_EQ(count753recursive(3357), 7);
+    // EXPECT_EQ(count753permutation(3357), 7); // TODO
 }
 
 TEST(TestCase, Negative) {
-    EXPECT_THROW(count753(-1), std::invalid_argument);
+    EXPECT_THROW(count753recursive(-1), std::invalid_argument);
+    EXPECT_THROW(count753permutation(-1), std::invalid_argument);
 }
 
