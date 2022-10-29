9b459f49a698bd90b58cb134c3e89489a82eccd1
skkzsh <skkzsh12@gmail.com>
2022-10-29 17:15:14 +0900

    Solve 5.5

--- my/q05_05.cpp
+++ my/q05_05.cpp
@@ -7,17 +7,11 @@ bool partial_sum_exists_pull(const int W, const vector<int> a) {
 
     // 初期条件
     dp[0][0] = true;
-    // for (int w = 1; w <= W; ++w) {
-    //     dp[0][w] = false;
-    // }
-
-    // a[n]を選ぶ場合
-    // n - 1 のときw - a[n]でtrueならtrue
 
     // もらう方式
     for (int n = 0; n < N; ++n) {
         for (int w = 0; w <= W; ++w) {
-            if(dp[n][w] || (a[n] <= w && dp[n][w - a[n]])) {
+            if(dp[n][w] || (a[n] <= w && dp[n + 1][w - a[n]])) {
                 dp[n + 1][w] = true;
             }
         }
@@ -32,17 +26,16 @@ bool partial_sum_exists_push(const int W, const vector<int> a) {
 
     // 初期条件
     dp[0][0] = true;
-    // for (int w = 1; w <= W; ++w) {
-    //     dp[0][w] = false;
-    // }
 
     // 配る方式
-    for (int n = 0; n < N; ++n) {
+    for (int n = 0; n <= N; ++n) {
         for (int w = 0; w <= W; ++w) {
             if(dp[n][w]) {
-                dp[n + 1][w] = true;
-                if(w + a[n] <= W) {
-                    dp[n + 1][w + a[n]] = true;
+                if (n < N) {
+                    dp[n + 1][w] = true;
+                }
+                if (w + a[n] <= W) {
+                    dp[n][w + a[n]] = true;
                 }
             }
         }
@@ -53,13 +46,13 @@ bool partial_sum_exists_push(const int W, const vector<int> a) {
 
 
 TEST(TestCase, TestTrue) {
-    const vector<int> a = {1, 2, 4, 5, 11};
+    const vector<int> a = {1};
     EXPECT_EQ(partial_sum_exists_pull(10, a), true);
     EXPECT_EQ(partial_sum_exists_push(10, a), true);
 }
 
 TEST(TestCase, TestFalse) {
-    const vector<int> a = {1, 5, 8, 11};
+    const vector<int> a = {3, 6, 8, 9, 11};
     EXPECT_EQ(partial_sum_exists_pull(10, a), false);
     EXPECT_EQ(partial_sum_exists_push(10, a), false);
 }
