16d01fef084a626f0eda9ad278e2899cbf789ae0
skkzsh <skkzsh12@gmail.com>
2022-11-04 19:50:38 +0900

    Test 5.8

---
+++ my/q05_08.cpp
@@ -0,0 +1,52 @@
+#include "gtest/gtest.h"
+#include "template.hpp"
+#include <numeric>
+using std::vector;
+
+double aqua(const int M, const vector<int> a) {
+    const int N = a.size();
+
+     if (M > N || M < 0) {
+         throw std::invalid_argument("0 <= M <= N required");
+     }
+
+    vector<vector<double>> dp(N + 1, vector<double>(M + 1, -1.0));
+
+    // 初期条件
+    dp[0][0] = 0;
+
+    for (int n = 1; n <= N; ++n) {
+        for (int m = 1; m <= std::min(n, M); ++m) {
+            dp[n][m] = (double) std::accumulate(a.begin(), a.begin() + n, 0) / n;
+        }
+    }
+
+    // debug
+    print2d(dp);
+
+    return dp[N][M];
+}
+
+TEST(TestCase, Sample1) {
+    const vector<int> a = {9, 1, 2, 3, 9};
+    EXPECT_EQ(aqua(3, a), 20);
+}
+
+TEST(TestCase, Sample2) {
+    const vector<int> a = {14, 4, 9, 7};
+    EXPECT_EQ(aqua(1, a), 8.5);
+}
+
+TEST(TestCase, Sample3) {
+    const vector<int> a = {11, 18, 9, 20, 4, 18, 12, 14};
+    EXPECT_EQ(aqua(3, a), 44.7);
+}
+
+TEST(TestCase, M_Negative) {
+    const vector<int> a = {9, 1, 2, 3, 9};
+    EXPECT_THROW(aqua(-1, a), std::invalid_argument);
+}
+TEST(TestCase, M_GreaterThan_N) {
+    const vector<int> a = {9, 1, 2, 3, 9};
+    EXPECT_THROW(aqua(6, a), std::invalid_argument);
+}
