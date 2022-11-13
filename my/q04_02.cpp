5aba41d551bba5c7b95d19f73a2c099e7dd4baf2
skkzsh <skkzsh12@gmail.com>
2022-11-13 18:57:28 +0900

    Use optional for memo 4.2

--- my/q04_02.cpp
+++ my/q04_02.cpp
@@ -1,13 +1,16 @@
 #include "gtest/gtest.h"
+#include <algorithm>
+using std::vector;
+using std::optional;
 
-long long tribo(const int N, std::vector<long long> &memo) {
+long long tribo(const int N, vector<optional<long long>> &memo) {
     if (N < 0) {
         throw std::invalid_argument("argument must not be negative");
     }
 
     // すでに計算済みならば解をリターン
-    if (memo[N] != -1) {
-        return memo[N];
+    if (memo[N].has_value()) {
+        return memo[N].value();
     }
 
     // ベースケース
@@ -22,33 +25,39 @@ long long tribo(const int N, std::vector<long long> &memo) {
             memo[N] = tribo(N - 1, memo) + tribo(N - 2, memo) + tribo(N - 3, memo); break;
     }
 
-    return memo[N];
+    return memo[N].value();
 }
 
 
-std::vector<long long> tribo_wrapper(const int N) {
+vector<long long> tribo_wrapper(const int N) {
     // tribo(N) の解をメモ化する配列
-    std::vector<long long> memo(N + 1, -1);
+    vector<optional<long long>> memo(N + 1);
 
     tribo(N, memo);
 
-    return memo;
+    // optionalはがし
+    vector<long long> result(N + 1);
+    std::ranges::transform(memo, result.begin(),
+        [](optional<long long> x) { return x.value(); }
+    );
+
+    return result;
 }
 
 TEST(TestCase, Test10) {
-    std::vector<long long> memo = tribo_wrapper(10);
+    vector<long long> result = tribo_wrapper(10);
 
-    EXPECT_EQ(memo[10], 81);
-    EXPECT_EQ(memo[9], 44);
-    EXPECT_EQ(memo[8], 24);
-    EXPECT_EQ(memo[7], 13);
-    EXPECT_EQ(memo[6], 7);
-    EXPECT_EQ(memo[5], 4);
-    EXPECT_EQ(memo[4], 2);
-    EXPECT_EQ(memo[3], 1);
-    EXPECT_EQ(memo[2], 1);
-    EXPECT_EQ(memo[1], 0);
-    EXPECT_EQ(memo[0], 0);
+    EXPECT_EQ(result[10], 81);
+    EXPECT_EQ(result[9], 44);
+    EXPECT_EQ(result[8], 24);
+    EXPECT_EQ(result[7], 13);
+    EXPECT_EQ(result[6], 7);
+    EXPECT_EQ(result[5], 4);
+    EXPECT_EQ(result[4], 2);
+    EXPECT_EQ(result[3], 1);
+    EXPECT_EQ(result[2], 1);
+    EXPECT_EQ(result[1], 0);
+    EXPECT_EQ(result[0], 0);
 }
 
 TEST(TestCase, Negative) {
