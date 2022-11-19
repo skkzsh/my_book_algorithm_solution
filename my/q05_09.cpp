8db1fde5ee82935a5931e2646d53a0cde70be5bb
skkzsh <skkzsh12@gmail.com>
2022-11-19 23:15:00 +0900

    (WIP) 答えを見た 5.9

---
+++ my/q05_09.cpp
@@ -0,0 +1,48 @@
+#include "gtest/gtest.h"
+#include "template.hpp"
+#include <numeric>
+#include <algorithm>
+using std::vector;
+
+// TODO: 実装
+int min_union_cost(const vector<int> slimes) {
+    using std::accumulate;
+    using std::next;
+
+    constexpr int INF = 1 << 29; // 十分大きな値
+    const int N = slimes.size();
+    vector<vector<int>> dp(N + 1, vector<int>(N + 1, INF));
+
+    // 初期条件
+    for (int i = 0; i < N; ++i) {
+        dp[i][i + 1] = 0;
+    }
+
+    // もらう方式
+    for (int i = 0; i < N - 1; ++i) {
+        for (int j = i + 2; j <= N; ++j) {
+            vector<int> tmp(j - i, INF);
+            for (int k = 0; k < j - i - 1; ++k) {
+                tmp[k] = dp[i][i + 1 + k] + dp[i + 1 + k][j] + accumulate(next(slimes.begin(), i), next(slimes.begin(), j), 0);
+            }
+            dp[i][j] = *std::ranges::min_element(tmp);
+        }
+    }
+
+    // debug
+    print2d(dp);
+
+    return dp[0][N];
+}
+
+TEST(TestCase, Ex1) {
+    EXPECT_EQ(min_union_cost({10, 20, 30, 40}), 190);
+}
+
+//TEST(TestCase, Ex2) {
+//    EXPECT_EQ(min_union_cost({10, 10, 10, 10}), 120);
+//}
+
+//TEST(TestCase, Ex4) {
+//    EXPECT_EQ(min_union_cost({7, 6, 8, 6, 1, 1}), 68);
+//}
