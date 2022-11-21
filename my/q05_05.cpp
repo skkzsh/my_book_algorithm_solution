17e0a8a43b1d717fb8c8ee94fb48e56fc18a5070
skkzsh <skkzsh12@gmail.com>
2022-11-21 21:16:15 +0900

    ifの判定式をそのまま代入

--- my/q05_05.cpp
+++ my/q05_05.cpp
@@ -11,9 +11,7 @@ bool partial_sum_exists_pull(const int W, const vector<int> a) {
     // もらう方式
     for (int n = 0; n < N; ++n) {
         for (int w = 0; w <= W; ++w) {
-            if (dp[n][w] || (a[n] <= w && dp[n + 1][w - a[n]])) {
-                dp[n + 1][w] = true;
-            }
+            dp[n + 1][w] = dp[n][w] || (a[n] <= w && dp[n + 1][w - a[n]]);
         }
     }
 
