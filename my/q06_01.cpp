712a5c861714b2e1c434a25fda5660f580107e31
skkzsh <skkzsh12@gmail.com>
2022-12-29 22:09:34 +0900

    Solve 6.1

---
+++ my/q06_01.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <ranges>
+using std::vector;
+
+vector<int> ranking(const vector<int> a) {
+    using namespace std::ranges;
+    const int N = a.size();
+
+    // aをsortしたvector
+    vector<int> sorted = a;
+    sort(sorted);
+
+    vector<int> result(N);
+
+    for (int i : std::views::iota(0, N)) {
+        result[i] = find(sorted, a[i]) - sorted.begin();
+//        result[i] = find(a, sorted[i]) - a.begin();
+    }
+
+    return result;
+}
+
+TEST(TestCase, Ex) {
+    const vector<int> result = ranking({12, 43, 7, 15, 9});
+    // sorted: 7, 9, 12, 15, 43
+
+    EXPECT_EQ(result[0], 2);
+    EXPECT_EQ(result[1], 4);
+    EXPECT_EQ(result[2], 0);
+    EXPECT_EQ(result[3], 3);
+    EXPECT_EQ(result[4], 1);
+}
