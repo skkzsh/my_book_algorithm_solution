7da23997a71b18b9865ec87a94689d60d3f4d204
skkzsh <skkzsh12@gmail.com>
2022-12-30 00:13:59 +0900

    答えを見た 6.2
    O(N log N)

--- my/q06_02.cpp
+++ my/q06_02.cpp
@@ -1,8 +1,10 @@
 #include "gtest/gtest.h"
 #include <algorithm>
+#include <ranges>
 using std::vector;
 
-int festival(const vector<vector<int>> z) {
+// O(N ^ 3)
+int festival_simple(const vector<vector<int>> z) {
     int count = 0;
 
     for (int a : z[0]) {
@@ -21,6 +23,26 @@ int festival(const vector<vector<int>> z) {
     return count;
 }
 
+// O(N log N)
+int festival_binary(vector<vector<int>> z) {
+    using namespace std::ranges;
+
+    for (int i : std::views::iota(0, 3)) {
+        sort(z[i]);
+    }
+
+    int count = 0;
+
+    for (const int b : z[1]) {
+        count += (lower_bound(z[0], b) - z[0].begin()) // a[i] >= b となる最小のi <=> a[i - 1] < b <= a[i] => 数は (i - 1) - 0 + 1 = iコ
+                    * (z[2].end() - upper_bound(z[2], b)); // c[i] > b となる最小のi => 数は N - i + 1 => c.end() - i
+        // count += (lower_bound(z[0], b) - z[0].begin())
+        //             * (N - (upper_bound(z[2], b) - z[2].begin()));
+    }
+
+    return count;
+}
+
 
 TEST(TestCase, Ex1) {
     const vector<vector<int>> z {
@@ -29,7 +51,8 @@ TEST(TestCase, Ex1) {
         {3, 6},
     };
 
-    EXPECT_EQ(festival(z), 3);
+    EXPECT_EQ(festival_simple(z), 3);
+    EXPECT_EQ(festival_binary(z), 3);
 }
 
 TEST(TestCase, Ex2) {
@@ -39,7 +62,8 @@ TEST(TestCase, Ex2) {
         {3, 3, 3},
     };
 
-    EXPECT_EQ(festival(z), 27);
+    EXPECT_EQ(festival_simple(z), 27);
+    EXPECT_EQ(festival_binary(z), 27);
 }
 
 TEST(TestCase, Ex3) {
@@ -49,5 +73,6 @@ TEST(TestCase, Ex3) {
         {2643, 383, 2, 79, 50, 288},
     };
 
-    EXPECT_EQ(festival(z), 87);
+    EXPECT_EQ(festival_simple(z), 87);
+    EXPECT_EQ(festival_binary(z), 87);
 }
