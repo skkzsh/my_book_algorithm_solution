80f3029fff79e170c173c42bc10d8f3cbf96d2f0
skkzsh <skkzsh12@gmail.com>
2022-12-30 03:21:41 +0900

    Solve 6.3
    O(N^4) 総当り

--- my/q06_03.cpp
+++ my/q06_03.cpp
@@ -1,8 +1,28 @@
 #include "gtest/gtest.h"
+#include "template.hpp"
 #include <algorithm>
 #include <cmath>
 
-int darts(const std::vector<int> a, const int M) {
+int darts_simple(const std::vector<int> a, const int M) {
+    int max = -1;
+
+    for (const int p : a) {
+        for (const int q : a) {
+            for (const int r : a) {
+                for (const int s : a) {
+                    const int tmp = p + q + r + s;
+                    if (tmp < M) {
+                        chmax(max, tmp);
+                    }
+                }
+            }
+        }
+    }
+
+    return max;
+}
+
+int darts_binary(const std::vector<int> a, const int M) {
     using namespace std::ranges;
     constexpr int L = 4;
     std::vector<int> aa(std::pow(a.size(), L));
@@ -22,5 +42,6 @@ int darts(const std::vector<int> a, const int M) {
 }
 
 TEST(TestCase, Ex1) {
-    EXPECT_EQ(darts({3, 14, 15, 9}, 50), 48);
+    EXPECT_EQ(darts_simple({3, 14, 15, 9}, 50), 48);
+    EXPECT_EQ(darts_binary({3, 14, 15, 9}, 50), 48);
 }
