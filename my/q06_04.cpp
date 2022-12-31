9259a8a135fcb4ecaea17c1318949497e0af4241
skkzsh <skkzsh12@gmail.com>
2022-12-31 14:37:35 +0900

    答えを見た 6.4

--- my/q06_04.cpp
+++ my/q06_04.cpp
@@ -1,7 +1,5 @@
 #include "gtest/gtest.h"
-#include <algorithm>
 #include <ranges>
-#include <numeric>
 
 int cows(const std::vector<int> a, const int M) {
     const int N = a.size();
@@ -10,29 +8,49 @@ int cows(const std::vector<int> a, const int M) {
         throw std::invalid_argument("2 <= M <= N required");
     }
 
-    const int M_dash = N;
-    std::vector<int> indexes(M_dash);
-    std::iota(indexes.begin(), indexes.end(), 0);
-    std::vector<int> distances(M_dash - 1);
+    // 元の問題: 「選んだMコの小屋のうち2つの小屋の距離の最小値」の最大値を求める
+    // <=> 判定問題: 「選んだMコの小屋のうち2つの小屋の距離の最小値」をx以上にすることができるか
+    // <=> 判定問題: 選んだMコの小屋のすべての距離をx以上にすることができるか
+    // <=> 判定問題: すべての小屋の距離をx以上にして, 小屋をMコを選べるか
 
-    for (const int i : std::views::iota(0, M_dash - 1)) {
-        distances[i] = a[indexes[i + 1]] - a[indexes[i]];
+    int left = 0; // 常にtrue
+    int right = a[N - 1] + 1; // 常にfalse
+
+    while (right - left > 1) {
+        const int x = (left + right) / 2;
+
+        int prev = 0; // 前回選んだ小屋のindex
+        int count = 1; // 小屋を選んだ個数
+
+        // 貪欲法
+        for (const int i : std::views::iota(0, N)) {
+            if (a[i] - a[prev] >= x) {
+                count++;
+                prev = i;
+            };
+        }
+
+        if (count >= M) { // Mコ以上選べた => もっと距離を離せる => xはもっと大きい (true領域を更新)
+            left = x;
+        } else {  // Mコ選べなかった => 距離を離せていない => xはもっと小さい (false領域を更新)
+            right = x;
+        }
     }
 
-    return *std::ranges::min_element(distances);
+    return left;
 }
 
-//TEST(TestCase, Ex) {
-//    EXPECT_EQ(cows({1, 2, 4, 8, 9}, 3), 3);
-//}
+TEST(TestCase, Ex) {
+    EXPECT_EQ(cows({1, 2, 4, 8, 9}, 3), 3);
+}
 
 TEST(TestCase, M_less_than_2) {
     EXPECT_THROW(cows({1, 2, 4, 8, 9}, 1), std::invalid_argument);
 }
 
-//TEST(TestCase, M_equal_to_2) {
-//    EXPECT_EQ(cows({1, 2, 4, 8, 9}, 2), 8);
-//}
+TEST(TestCase, M_equal_to_2) {
+    EXPECT_EQ(cows({1, 2, 4, 8, 9}, 2), 8);
+}
 
 TEST(TestCase, M_equal_to_N) {
     EXPECT_EQ(cows({1, 2, 4, 8, 9}, 5), 1);
