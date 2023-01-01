756ab0ade3148ae65edb688332d6f32d21a45f51
skkzsh <skkzsh12@gmail.com>
2023-01-01 17:18:02 +0900

    答えを見た 6.5
    O(N * log N * log C)

--- my/q06_05.cpp
+++ my/q06_05.cpp
@@ -1,23 +1,69 @@
 #include "gtest/gtest.h"
 #include <algorithm>
 using std::vector;
+using namespace std::ranges;
 
-// O(N^2 log N)
-int product_th(const vector<int> a, const vector<int> b, const int K) {
+// O(N^2 * log N)
+int product_th_simple(const vector<int> a, const vector<int> b, const int K) {
     vector<int> p; // (a.size() * b.size())
     for (const int i : a) {
         for (const int j : b) {
             p.push_back(i * j);
         }
     }
-    std::ranges::sort(p);
+    sort(p);
     return p[K - 1];
 }
 
+// O(N * log N * log C)
+int product_th_binary(vector<int> a, vector<int> b, const int K) {
+    // 元の問題: a[i] * b[j] の中でK番目に小さい値をを求める
+    // <=> 判定問題: a[i] * b[j] のうちx以下がKコ (以上) あるかどうか
+    // a[i] * b[j] <= x
+    // a[i] を固定して b[j] <= x / a[i] となる最大のjを求める
+    // <=> a[i] を固定して b[j] > x / a[i] となる最小のjを求める
+    // <=> b[j - 1] <= x / a[i] < b[j]
+    // j - 1 までで (j - 1) + 1 = jコ
+
+    sort(a);
+    sort(b);
+
+    if (a[0] <= 0 || b[0] <= 0) {
+        throw std::invalid_argument("a and b must be positive numbers");
+    }
+
+    int left = 0; // 常にfalse (∵ a, b > 0)
+    int right = a[a.size() - 1] * b[b.size() - 1]; // 常にtrue
+
+    while (right - left > 1) {
+        const int x = (left + right) / 2;
+
+        int count = 0;
+        for (const int k : a) {
+            count += upper_bound(b, x / k) - b.begin();
+        }
+
+        if (count >= K) {
+            right = x;
+        } else {
+            left = x;
+        }
+    }
+
+    return right;
+}
+
 TEST(TestCase, Ex1) {
-    EXPECT_EQ(product_th({2, 3}, {3, 5}, 3), 10);
+    EXPECT_EQ(product_th_simple({2, 3}, {3, 5}, 3), 10);
+    EXPECT_EQ(product_th_binary({2, 3}, {3, 5}, 3), 10);
 }
 
 TEST(TestCase, Ex2) {
-    EXPECT_EQ(product_th({1, 2, 1}, {2, 1, 2}, 7), 2);
+    EXPECT_EQ(product_th_simple({1, 2, 1}, {2, 1, 2}, 7), 2);
+    EXPECT_EQ(product_th_binary({1, 2, 1}, {2, 1, 2}, 7), 2);
+}
+
+TEST(TestCase, Zero) {
+    EXPECT_THROW(product_th_binary({1, 0, 1}, {1, 1, 1}, 1), std::invalid_argument);
+    EXPECT_THROW(product_th_binary({1, 1, 1}, {1, 1, 0}, 1), std::invalid_argument);
 }
