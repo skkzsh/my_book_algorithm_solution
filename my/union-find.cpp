c400636a77288b1fd967793b23d7a0ef565d69f7
skkzsh <skkzsh12@gmail.com>
2023-02-23 14:30:53 +0900

    集合の数を求める関数

--- my/union-find.cpp
+++ my/union-find.cpp
@@ -1,12 +1,13 @@
 #include "union-find.hpp"
-#include <stdexcept>
+#include <ranges>
+#include <algorithm>
 #include <utility>
 
-UnionFind::UnionFind(const int n) : parents(n), sizes(n, 1) {}
+UnionFind::UnionFind(const int N) : N(N), parents(N), sizes(N, 1) {}
 
 int UnionFind::root(const int v) {
     if (!parents.at(v).has_value()) {
-        return v;
+        return v;  // 親がいないなら, 自身が根
     }
 
     parents.at(v) = root(parents.at(v).value()); // 経路圧縮
@@ -14,14 +15,14 @@ int UnionFind::root(const int v) {
 }
 
 bool UnionFind::is_same_set(const int u, const int v) {
-    return root(u) == root(v);
+    return root(u) == root(v);  // 根が同じなら同じ集合
 }
 
 bool UnionFind::unite(const int u, const int v) {
     int root_u = root(u);
     int root_v = root(v);
 
-    if (root_u == root_v) {
+    if (root_u == root_v) {  // すでに同じ集合なら何もしない
         return false;
     }
 
@@ -47,4 +48,14 @@ bool UnionFind::unite(const int u, const int v) {
 
 int UnionFind::size(const int v) {
     return sizes[root(v)];
+}
+
+int UnionFind::count_set() const {
+    using std::ranges::count_if;
+    using std::views::iota;
+
+    // 根の数を数える
+    return count_if(iota(0, N), [this](const int v){
+        return !parents.at(v).has_value(); // 親がいないなら, 自身が根
+    });
 }
--- my/union-find.hpp
+++ my/union-find.hpp
@@ -7,13 +7,18 @@ using std::optional;
 
 class UnionFind {
     private:
+        const int N; // 要素数
         vector<optional<int>> parents;
-        vector<int> sizes;
+        vector<int> sizes; // 各要素の属する集合の要素数
     public:
-        UnionFind(const int n);
+        UnionFind(const int N);
+    private:
         int root(const int v);
+    public:
         bool is_same_set(const int u, const int v);
         bool unite(const int u, const int v);
     private:
-        int size(int v);
+        int size(const int v); // vの属する集合の要素数
+    public:
+        int count_set() const; // 集合の数
 };
