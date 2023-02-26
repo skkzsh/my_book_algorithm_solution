fc3710aa95d3f4617404cd81651aca2b0e87d3c6
skkzsh <skkzsh12@gmail.com>
2023-02-26 20:36:15 +0900

    Arrange Union-Find

--- my/union-find.hpp
+++ my/union-find.hpp
@@ -10,13 +10,11 @@ class UnionFind {
         const int N; // 要素数
         vector<optional<int>> parents;
         vector<int> sizes; // 各要素の属する集合の要素数
+        int size(const int v); // vの属する集合の要素数
     public:
         UnionFind(const int N);
         int root(const int v);
         bool is_same_set(const int u, const int v);
         bool unite(const int u, const int v);
-    private:
-        int size(const int v); // vの属する集合の要素数
-    public:
         int count_set() const; // 集合の数
 };
