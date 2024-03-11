4b4045be042141a00c0147af3ed24e44728031ce
skkzsh <skkzsh12@gmail.com>
2024-03-11 23:06:11 +0900

    13.4 s-t path (recursive search)

--- my/code13_04.cpp
+++ my/code13_04.cpp
@@ -1,33 +1,54 @@
 #include "gtest/gtest.h"
 #include "graph.hpp"
+#include <queue>
 #include <stack>
 using std::pair;
 
 // E: 辺集合
 // p: {s, t}
-bool exists_path(const Pairs<int> &E, const pair<int, int> &p) {
-  using std::vector;
 
+void recursive_search(const vector<vector<int>> &G, const int v, vector<bool> &seen) {
+  seen.at(v)= true;
+
+  for (const auto n : G.at(v)) {
+    if (!seen.at(n)) {
+      recursive_search(G, n, seen);
+    }
+  }
+}
+
+bool exists_path_by_recursive(const Pairs<int> &E, const pair<int, int> &p) {
   const vector<vector<int>> G = to_adjacency_list(E, true);
   const auto [s, t] = p;
 
   vector<bool> seen(G.size());
-  std::stack<int> todo;
+
+  recursive_search(G, s, seen);
+
+  return seen.at(t);
+}
+
+bool exists_path_by_bfs(const Pairs<int> &E, const pair<int, int> &p) {
+  const vector<vector<int>> G = to_adjacency_list(E, true);
+  const auto [s, t] = p;
+
+  vector<bool> seen(G.size());
+  std::queue<int> todo; // BFS
 
   seen.at(s) = true;
   todo.push(s);
 
   while (!todo.empty()) {
-    const auto v = todo.top();
+    const auto v = todo.front();
     todo.pop();
 
-    for (const auto x : G.at(v)) {
-      if (x == t) {
+    for (const auto n : G.at(v)) {
+      if (n == t) {
         return true;
       }
-      if (!seen.at(x)) {
-        seen.at(x) = true;
-        todo.push(x);
+      if (!seen.at(n)) {
+        seen.at(n) = true;
+        todo.push(n);
       }
     }
 
@@ -53,5 +74,6 @@ TEST(TestSuite, Ex) {
       {6, 7},
     };
 
-  EXPECT_TRUE(exists_path(E, {0, 7}));
+  EXPECT_TRUE(exists_path_by_recursive(E, {0, 7}));
+  EXPECT_TRUE(exists_path_by_bfs(E, {0, 7}));
 }
