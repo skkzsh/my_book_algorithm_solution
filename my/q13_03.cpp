d1ff96b11e388720a1232d56a26eecf6a6681baa
skkzsh <skkzsh12@gmail.com>
2024-03-14 20:52:14 +0900

    13.3 二部グラフ (BFS)

--- my/q13_03.cpp
+++ my/q13_03.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 #include "graph.hpp"
+#include <queue>
+#include <stack>
 using std::optional;
 
 // 2部グラフの分類
@@ -34,23 +36,56 @@ bool is_bipartite_by_recursive(const multimap<int, int> &E) {
   return recursive_search(G, 0, RED, colors);
 }
 
+bool is_bipartite_by_bfs(const multimap<int, int> &E) {
+  const auto G = to_adjacency_list(E);
+
+  vector<optional<COLOR>> colors(G.size());
+  std::queue<int> todo; // BFS
+
+  colors.at(0) = RED;
+  todo.push(0);
+
+  while (!todo.empty()) {
+    const auto v = todo.front();
+    todo.pop();
+    const auto curr = colors.at(v);
+
+    for (const auto n: G.at(v)) {
+      if (colors.at(n).has_value()) {
+        if (colors.at(n).value() == curr) {
+          return false;
+        }
+      } else {
+        colors.at(n) = curr == RED ? BLUE : RED;
+        todo.push(n);
+      }
+    }
+  }
+
+  return true;
+}
+
 TEST(TestSuite, Bipartite) {
-  EXPECT_TRUE(is_bipartite_by_recursive({
+  const multimap<int, int> E {
     {0, 1},
     {0, 3},
     {1, 2},
     {1, 4},
     {3, 4},
-    }));
+  };
+  EXPECT_TRUE(is_bipartite_by_recursive(E));
+  EXPECT_TRUE(is_bipartite_by_bfs(E));
 }
 
 TEST(TestSuite, NotBipartite) {
-  EXPECT_FALSE(is_bipartite_by_recursive({
+  const multimap<int, int> E {
     {0, 1},
     {0, 3},
     {1, 2},
     {1, 4},
     {3, 4},
     {2, 4}, // 2部グラフじゃなくする
-    }));
+  };
+  EXPECT_FALSE(is_bipartite_by_recursive(E));
+  EXPECT_FALSE(is_bipartite_by_bfs(E));
 }
