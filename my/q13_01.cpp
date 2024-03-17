c6c554700e9b47db210d743cc0fbeacc015042db
skkzsh <skkzsh12@gmail.com>
2024-03-17 18:20:39 +0900

    13.1 TEST_P

--- my/q13_01.cpp
+++ my/q13_01.cpp
@@ -20,44 +20,58 @@ int count_connected(const multimap<int, int> &E, auto search) {
   return count;
 }
 
-TEST(TestSuite, Connected) {
-  const multimap<int, int> E {
-    {0, 1},
-    {0, 2},
-    {0, 4},
-    {1, 3},
-    {1, 4},
-    {1, 8},
-    {2, 5},
-    {3, 7},
-    {3, 8},
-    {4, 8},
-    {5, 6},
-    {5, 8},
-    {6, 7},
-  };
 
-  EXPECT_EQ(count_connected(E, recursive_search), 1);
-  EXPECT_EQ(count_connected(E, bfs), 1);
+const struct TestParam {
+  const multimap<int, int> E;
+  const int gold;
+} PARAMS[] {
+  {
+    {
+      {0, 1},
+      {0, 2},
+      {0, 4},
+      {1, 3},
+      {1, 4},
+      {1, 8},
+      {2, 5},
+      {3, 7},
+      {3, 8},
+      {4, 8},
+      {5, 6},
+      {5, 8},
+      {6, 7},
+    },
+    1,
+  },
+  {
+    {
+      {0, 1},
+      // {0, 2}, // (2, 5) を孤立させる
+      {0, 4},
+      {1, 3},
+      {1, 4},
+      {1, 8},
+      {2, 5},
+      // {3, 7}, // (6, 7) を孤立させる
+      {3, 8},
+      {4, 8},
+      // {5, 6}, // (6, 7) を孤立させる
+      // {5, 8}, // (2, 5) を孤立させる
+      {6, 7},
+    },
+    3,
+  }
+};
+
+class TestSuite : public ::testing::TestWithParam<TestParam> {};
+
+TEST_P(TestSuite, Ex) {
+  EXPECT_EQ(count_connected(GetParam().E, recursive_search), GetParam().gold);
+  EXPECT_EQ(count_connected(GetParam().E, bfs), GetParam().gold);
 }
 
-TEST(TestSuite, NotConnected) {
-  const multimap<int, int> E{
-    {0, 1},
-    // {0, 2}, // (2, 5) を孤立させる
-    {0, 4},
-    {1, 3},
-    {1, 4},
-    {1, 8},
-    {2, 5},
-    // {3, 7}, // (6, 7) を孤立させる
-    {3, 8},
-    {4, 8},
-    // {5, 6}, // (6, 7) を孤立させる
-    // {5, 8}, // (2, 5) を孤立させる
-    {6, 7},
-  };
-
-  EXPECT_EQ(count_connected(E, recursive_search), 3);
-  EXPECT_EQ(count_connected(E, bfs), 3);
-}
+INSTANTIATE_TEST_SUITE_P(
+    Inst,
+    TestSuite,
+    ::testing::ValuesIn(PARAMS)
+                         );
