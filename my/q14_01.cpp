13836071480dd804b2f13cc03852d4a6a6059d2a
skkzsh <skkzsh12@gmail.com>
2026-01-18 20:19:25 +0900

    Test longest path

---
+++ my/q14_01.cpp
@@ -0,0 +1,57 @@
+#include "gtest/gtest.h"
+#include "graph.hpp"
+
+// E: 辺集合
+constexpr int longest_path(const multimap<int, int> &E) {
+  // TODO: 実装
+  return 2;
+}
+
+const struct TestParam {
+  const multimap<int, int> E;
+  const int gold;
+} PARAMS[] {
+  {
+    {
+      {1, 2},
+      {1, 3},
+      {3, 2},
+      {2, 4},
+      {3, 4},
+    },
+    3,
+  },
+  {
+    {
+      {2, 3},
+      {4, 5},
+      {5, 6},
+    },
+    2,
+  },
+  {
+    {
+      {5, 3},
+      {2, 3},
+      {2, 4},
+      {5, 2},
+      {5, 1},
+      {1, 4},
+      {4, 3},
+      {1, 3},
+    },
+    3,
+  },
+};
+
+class TestSuite : public testing::TestWithParam<TestParam> {};
+
+TEST_P(TestSuite, Ex) {
+  EXPECT_EQ(longest_path(GetParam().E), GetParam().gold);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+  Inst,
+  TestSuite,
+  testing::ValuesIn(PARAMS)
+);
