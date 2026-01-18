2d74e68c173e2755ff762cdd2083434516a6dda2
skkzsh <skkzsh12@gmail.com>
2026-01-18 14:29:38 +0900

    Dijkstra

--- my/code14_03.cpp
+++ my/code14_03.cpp
@@ -9,16 +9,42 @@ using ::testing::ElementsAreArray;
 // s: 始点
 constexpr vector<int> dijkstra(const map<pair<int, int>, int> &E, const int s) {
   using std::views::iota;
+  using std::optional;
   constexpr int INF = 1 << 29; // 十分大きな値
 
   const auto G = to_adjacency_list(E);
   // debug
   // print2d(G);
 
+  vector<bool> used(G.size());
   vector<int> dists(G.size(), INF);
   dists.at(s) = 0;
 
-  // TODO: 実装
+  for (const auto i : iota(0u, G.size())) {
+    // 使用済でない頂点のうち, distsが最小の頂点を探す
+    // min_elementは使えない?
+    int min_dist = INF;
+    optional<int> min_v;
+
+    for (const auto v : iota(0u, G.size())) {
+      if (!used.at(v) && dists.at(v) < min_dist) {
+        min_dist = dists.at(v);
+        min_v = v;
+      }
+    }
+    // debug
+    // std::println("{}: {}", min_v.value(), min_dist);
+
+    if (min_v.has_value()) {
+      // 最小の頂点があれば, 使用済とする
+      used.at(min_v.value()) = true;
+
+      // 最小の頂点を始点とした各辺を緩和する
+      for (const auto &[to, weight] : G.at(min_v.value())) {
+        chmin(dists.at(to), dists.at(min_v.value()) + weight);
+      }
+    }
+  }
 
   return dists;
 }
